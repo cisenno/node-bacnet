0eda794d9c43c2e448de7faa11e4bb2aa30372bc
Klaus Landsdorf (mac) <klaus.landsdorf@bianco-royal.de>
2020-05-17 21:11:04 +0200

    refactor: clean code from copy and paste errors, but there is more

--- test/integration/who-is.spec.js
+++ test/integration/who-is.spec.js
@@ -6,7 +6,7 @@ const utils       = require('./utils');
 describe('bacstack - whoIs integration', () => {
   it('should not invoke a event if no device is available', (next) => {
     const client = new utils.bacnetClient({apduTimeout: 200});
-    client.on('iAm', (address, deviceId, maxApdu, segmentation, vendorId) => {
+    client.on('iAm', (receiver, deviceId, maxApdu, segmentation, vendorId) => {
       client.close();
       next(new Error('Unallowed Callback'));
     });
